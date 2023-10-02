02b61e6ff577c754b4245b8faf5b95397ca4233f
Islam Shamshiev <55310909+SakeXYZ@users.noreply.github.com>
2023-10-02 17:49:11 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 URL = "https://api.trello.com/1/"
-API_KEY = "78fead8869e6e747a95c924212fff638"
-TOKEN = "ATTA070b2bf9c6cb6aec104bd1ba59f195b586bbf8c0c3a82c875b31a05e8b0383cf9B8B39BD"
-ID = "6513e2ca2c3fe7539cf3227d"
+API_KEY = "api key trello"
+TOKEN = "trello token"
+ID = "your idlist in trello"
